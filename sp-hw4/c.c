e75f4124af88f715bc0e09e407088802f66c6665
Jeonhui <dlwjsgml02@naver.com>
2022-06-07 16:04:33 +0900

    hw4

--- sp-hw4/c.c
+++ sp-hw4/c.c
@@ -8,49 +8,46 @@
 #include <unistd.h>
 
 #define MAX 1024 // max KB input length
-#define PORT 30077 // server port #
-//#define HOSTADDR "192.168.16.30" // server IP
+#define PORT 32763// server port #
 #define HOSTADDR "127.0.0.1" // server IP
 
-int main (int argc, char *argv[])
-{
-	int sd, send_bytes, n, recv_bytes;
+int main(int argc, char *argv[]) {
+    int sd, send_bytes, n, recv_bytes;
 
-	struct sockaddr_in servaddr;
-	char snddata[MAX], rcvdata[MAX];
-	bzero ((char*) &servaddr, sizeof(servaddr)); // prepare server address, port
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr (HOSTADDR);
-	servaddr.sin_port = htons (PORT);
-	if (( sd = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
-		fprintf( stderr, "can’t open socket.\n");
-		exit(1);
-	}
+    struct sockaddr_in servaddr;
+    char snddata[1024];
+    char data[1024];
 
-	if (connect (sd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-		fprintf (stderr, "can’t connect to server.\n");
-		exit(1);
-	} // auto binding for this client and connect request
-	while (fgets (snddata, MAX, stdin) != NULL) { // get a string from KB
-		send_bytes = strlen (snddata);
-		if (send (sd, snddata, send_bytes, 0) != send_bytes) { // to server
-			fprintf( stderr, "can’t send data.\n");
-			exit(1);
-		}
+    bzero ((char *) &servaddr, sizeof(servaddr)); // prepare server address, port
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(HOSTADDR);
+    servaddr.sin_port = htons (PORT);
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        fprintf(stderr, "can’t open socket.\n");
+        exit(1);
+    }
 
-		recv_bytes = 0;
-		while (recv_bytes < send_bytes) { // while loop for stream I/O !
-			if ((n = recv (sd, rcvdata + recv_bytes, MAX, 0)) < 0) { // from server
-				fprintf (stderr, "can’t receive data.\n");
-				exit(1);
-			}
-			recv_bytes += n;
-		}
-		rcvdata[recv_bytes] = 0; // NULL char for string
-		printf("Echo: ");
-		fputs (rcvdata, stdout); // display
-		fflush(stdout);
-	}
-	close (sd);
-	return 0;
+    if (connect(sd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        fprintf(stderr, "can’t connect to server.\n");
+        exit(1);
+    } // auto binding for this client and connect request
+
+    while (fgets(snddata, MAX, stdin) != NULL) { // get a string from KB
+        send_bytes = strlen(snddata);
+
+        if (send(sd, snddata, send_bytes, 0) != send_bytes) { // to server
+            fprintf(stderr, "can’t send data.\n");
+            exit(1);
+        }
+
+        memset(data, 0, MAX);
+        recv_bytes = recv(sd, data, MAX, 0);
+        fputs(data, stdout); // display
+        fflush(stdout);
+        if(strcmp(data,"quit_ack") == 0)
+            break;
+        memset(data, 0, MAX);
+    }
+    close(sd);
+    return 0;
 }
--- sp-hw4/s.c
+++ sp-hw4/s.c
@@ -7,60 +7,171 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 
 #define MAX 1024
 #define PORT 32763
-#define BACKLOG 5
+#define BACKLOG 50
+#define HOSTADDR "127.0.0.1"
+#define DICTMAX 3
 
-int main (int argc, char *argv[])
-{
-	int sd, nsd, pid, bytes, cliaddrsize;
-	struct sockaddr_in cliaddr, servaddr;
-	char data[MAX];
-	
-	if (( sd = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
-		fprintf( stderr, "can’t open socket.\n");
-		exit(1);
-	}
-	
-	bzero ((char*) &servaddr, sizeof( servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl (INADDR_ANY);
-	servaddr.sin_port = htons (PORT);
+typedef struct dictElement {
+    char key[50];
+    char value[50];
+} element;
 
-	if (bind (sd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-		fprintf (stderr, "can’t bind to socket.\n");
-		exit(1);
-	} // bind itself to the socket
-	
-	listen (sd, BACKLOG); // declare the client-queue length
+element Dictionary[DICTMAX];
 
-	while (1) { // a typical server waiting loop
-		cliaddrsize = sizeof (cliaddr);
-		if (( nsd = accept (sd, (struct sockaddr *) &cliaddr, &cliaddrsize)) < 0) {
-			fprintf (stderr, "can’t accept connection.\n");
-			exit(1);
-		} // upon return: client addr. is known and a new socket is created
-		if ((pid = fork()) < 0) { // fork error, a new thread may be used!
-			fprintf (stderr, "can’t fork process.\n"); exit(1);
-		}
+typedef struct threadData {
+    int cid;
+    int nsd;
+} ThreadData;
 
-		if (pid == 0) { // the new child server for the connected client
-			close (sd); // old socket is not necessary for me
-			while(1) {
-				bytes = recv (nsd, data, MAX, 0); // from client
-				if (bytes == 0) // client quit
-					break;
-				else if (bytes < 0) { // error
-					fprintf (stderr, "can’t receive data.\n"); exit(1);
-				}
-				if (send (nsd, data, bytes, 0) != bytes) { // echo back
-					fprintf (stderr, "can’t send data.\n"); exit(1);
-				}
-			} // end while, client quits
-			return 0; // child server exit.
-		} // else : parent
+int request(char *key, char *value, int mode) {
+    for (int i = 0; i < DICTMAX; i++) {
+        if (strcmp(Dictionary[i].key, key) == 0) {
+            if (mode == 0) {
+                strcpy(value, Dictionary[i].value);
+                return 1;
+            } else if (mode == 1) {
+                strcpy(Dictionary[i].value, value);
+                return 1;
+            } else if (mode == 2) {
+                return 0;
+            }
+        }
+        if (mode == 2 && strlen(Dictionary[i].key) == 0) {
+            strcpy(Dictionary[i].key, key);
+            strcpy(Dictionary[i].value, value);
+            return 1;
+        }
+    }
+    return 0;
+}
 
-		close (nsd); // parent: close the new socket
-	}
+
+void split(char *string, char *command, char *key, char *value) {
+    int cnt = 0;
+    int spl = 0;
+
+    for (int i = 0; i < strlen(string); i++) {
+        if (string[i] == ' ' || string[i] == '\n' || string[i] == '\0') {
+            if (cnt == 0)
+                command[i - spl] = '\0';
+            else if (cnt == 1)
+                key[i - spl] = '\0';
+            else if (cnt == 2)
+                value[i - spl] = '\0';
+            spl = i + 1;
+            cnt += 1;
+        } else {
+            if (cnt == 0)
+                command[i - spl] = string[i];
+            else if (cnt == 1)
+                key[i - spl] = string[i];
+            else if (cnt == 2)
+                value[i - spl] = string[i];
+        }
+    }
+}
+
+void *service(void *d) {
+    long bytes;
+    char data[MAX];
+    char sndData[MAX];
+    char command[10];
+    char key[50];
+    char value[50];
+
+    ThreadData *td = (ThreadData *) d;
+
+    int cid = td->cid;
+    int nsd = td->nsd;
+
+    while (1) {
+        memset(data, 0, MAX);
+        bytes = recv(nsd, data, MAX, 0);
+
+        if (bytes < 0) {
+            fprintf(stderr, "can’t receive data.\n");
+            pthread_exit(NULL);
+        }
+
+        printf("client %02d : %s", cid, data);
+
+        split(data, command, key, value);
+
+        memset(sndData, 0, MAX);
+
+        if (strcmp(command, "read") == 0) {
+            if (request(key, value, 0) == 1) {
+                sprintf(sndData, "%02d read: %s %s\n", cid, key, value);
+            } else {
+                sprintf(sndData, "%02d read Failed\n", cid);
+            }
+        } else if (strcmp(command, "update") == 0) {
+            if (request(key, value, 1) == 1) {
+                sprintf(sndData, "%02d update OK : %s %s\n", cid, key, value);
+            } else {
+                sprintf(sndData, "%02d update Failed\n", cid); //(key가 없는 경우)
+            }
+        } else if (strcmp(command, "insert") == 0) {
+            if (request(key, value, 2) == 1) {
+                sprintf(sndData, "%02d insert OK : %s %s\n", cid, key, value);
+            } else {
+                sprintf(sndData, "%02d insert Failed\n", cid);
+            }
+        } else if (strcmp(command, "quit") == 0) {
+            sprintf(sndData, "quit_ack");
+        } else {
+            strcpy(sndData, data);
+        }
+        if (send(nsd, sndData, strlen(sndData), 0) != strlen(sndData)) { // echo back
+            fprintf(stderr, "can’t send data.\n");
+            pthread_exit(NULL);
+        }
+
+        if (strcmp(command, "quit") == 0) {
+            break;
+        }
+    }
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    int sd, nsd;
+    pthread_t tid;
+    int clientNum = 1;
+    ThreadData td;
+    long cliaddrsize;
+    struct sockaddr_in cliaddr, servaddr;
+
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        fprintf(stderr, "can’t open socket.\n");
+        exit(1);
+    }
+
+    bzero ((char *) &servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(HOSTADDR);
+    servaddr.sin_port = htons (PORT);
+
+    if (bind(sd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        fprintf(stderr, "can’t bind to socket.\n");
+        exit(1);
+    }
+
+    listen(sd, BACKLOG);
+
+    while (1) {
+        cliaddrsize = sizeof(cliaddr);
+        if ((nsd = accept(sd, (struct sockaddr *) &cliaddr, (socklen_t *) &cliaddrsize)) < 0) {
+            fprintf(stderr, "can’t accept connection.\n");
+            exit(1);
+        } else {
+            td.cid = clientNum++;
+            td.nsd = nsd;
+            pthread_create(&tid, NULL, (void *) service, (void *) &td);
+        }
+    }
 }
